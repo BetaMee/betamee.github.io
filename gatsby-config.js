cac82d9c2f059bc1e2cfc3974c43250db34fc316
betamee <gongxq95@gmail.com>
2018-01-12 01:15:04 +0800

    finish tutorial

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,13 +10,13 @@ module.exports = {
         pathToConfigModule: `src/utils/typography.js`,
       },
     },
-    // `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
         path: `${__dirname}/src`
       }
-    }
+    },
+    `gatsby-transformer-remark`
   ],
 }
