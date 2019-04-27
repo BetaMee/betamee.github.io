469b03bc4799c0447a8f6fe86da5f3da19a2009d
betamee <gongxq95@gmail.com>
2019-04-27 22:44:17 +0800

    update

--- src/templates/BlogTemplate.jsx
+++ src/templates/BlogTemplate.jsx
@@ -4,40 +4,40 @@ import { graphql, Link } from 'gatsby'
 import Layout from '../components/layout'
 import styles from './blog.module.css'
 
-export default class BlogTemplate extends React.Component {
-  render() {
-    const article = this.props.data.markdownRemark
-    const site = this.props.data.site
-    return (
-      <Layout
-        site={site}
-      >
-        <div className={styles.content} >
-          <div className={styles.heading}>
-            <h1 className={styles.title}>
-              {article.frontmatter.title}
-            </h1>
-            <div className={styles.info}>
-              <span>{site.siteMetadata.author}</span>
-              <span>{article.frontmatter.date}</span>
-            </div>
-            <div className={styles.tags}>
-              {(article.frontmatter.tags || []).split(' ').map((_tag, index) => (
-                <Link key={index} to={`/tag/${_tag}`}>
-                  #{_tag}
-                </Link>
-              ))}
-            </div>
+const BlogTemplate = () => {
+  const markdownRemark = this.props.data.markdownRemark
+  const site = this.props.data.site
+  return (
+    <Layout
+      site={site}
+    >
+      <div className={styles.content} >
+        <div className={styles.heading}>
+          <h1 className={styles.title}>
+            {markdownRemark.frontmatter.title}
+          </h1>
+          <div className={styles.info}>
+            <span>{site.siteMetadata.author}</span>
+            <span>{markdownRemark.frontmatter.date}</span>
           </div>
-          <div dangerouslySetInnerHTML={{ __html: article.html }} />
+          <div className={styles.tags}>
+            {(markdownRemark.frontmatter.tags || []).split(' ').map((_tag, index) => (
+              <Link key={index} to={`/tag/${_tag}`}>
+                #{_tag}
+              </Link>
+            ))}
+          </div>
         </div>
-      </Layout>
-    )
-  }
+        <div dangerouslySetInnerHTML={{ __html: markdownRemark.html }} />
+      </div>
+    </Layout>
+  )
 }
 
+export default BlogTemplate
+
 export const blogTemplateQuery = graphql`
-  query articleBySlug($slug: String!) {
+  query blogBySlug($slug: String!) {
     site {
       siteMetadata {
         title
