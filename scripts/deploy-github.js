378f41e94174c9006e9573ef88c67f684dac5423
betamee <gongxq95@gmail.com>
2019-07-09 11:27:01 +0800

    clean master branch

--- scripts/deploy-github.js
+++
@@ -1,9 +0,0 @@
-const ghpages = require('gh-pages')
-
-ghpages.publish('public', {
-  branch: 'master',
-  repo: 'git@github.com:BetaMee/betamee.github.io.git'
-}, () => {
-  // eslint-disable-next-line no-console
-  console.log('Deploy Complete!')
-})
