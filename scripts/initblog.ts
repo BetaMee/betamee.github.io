d6d8031175ba5273422346ce264554aa7bddd527
betamee <gongxq95@gmail.com>
2019-10-19 14:47:23 +0800

    fix: update init script

--- scripts/initblog.ts
+++ scripts/initblog.ts
@@ -106,12 +106,12 @@ const createDate = () => {
   const date = new Date()
 
   const year = date.getFullYear()
-  const month = date.getMonth() > 10 ? (date.getMonth() + 1) : `0${date.getMonth() + 1}`
-  const day = date.getDate() > 10 ? date.getDate() : `0${date.getDate()}`
+  const month = date.getMonth() > 9 ? (date.getMonth() + 1) : `0${date.getMonth() + 1}`
+  const day = date.getDate() > 9 ? date.getDate() : `0${date.getDate()}`
 
-  const hour = date.getHours() > 10 ? date.getHours() : `0${date.getHours()}`
-  const min = date.getMinutes() > 10 ? date.getMinutes() : `0${date.getMinutes()}`
-  const sec = date.getSeconds() > 10 ? date.getSeconds() : `0${ date.getSeconds()}`
+  const hour = date.getHours() > 9 ? date.getHours() : `0${date.getHours()}`
+  const min = date.getMinutes() > 9 ? date.getMinutes() : `0${date.getMinutes()}`
+  const sec = date.getSeconds() > 9 ? date.getSeconds() : `0${ date.getSeconds()}`
 
   const createDate = `${year}-${month}-${day} ${hour}:${min}:${sec}` 
   return createDate
