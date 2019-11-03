e7433ef9ca44d043f929129b5a2c776b3b668b55
betamee <gongxq95@gmail.com>
2019-11-03 22:55:03 +0800

    fix: fix date init

--- scripts/initblog.ts
+++ scripts/initblog.ts
@@ -106,14 +106,14 @@ const createDate = () => {
   const date = new Date()
 
   const year = date.getFullYear()
-  const month = date.getMonth() > 9 ? (date.getMonth() + 1) : `0${date.getMonth() + 1}`
+  const month = date.getMonth() >= 9 ? (date.getMonth() + 1) : `0${date.getMonth() + 1}`
   const day = date.getDate() > 9 ? date.getDate() : `0${date.getDate()}`
 
   const hour = date.getHours() > 9 ? date.getHours() : `0${date.getHours()}`
   const min = date.getMinutes() > 9 ? date.getMinutes() : `0${date.getMinutes()}`
   const sec = date.getSeconds() > 9 ? date.getSeconds() : `0${ date.getSeconds()}`
 
-  const createDate = `${year}-${month}-${day} ${hour}:${min}:${sec}` 
+  const createDate = `${year}-${month}-${day} ${hour}:${min}:${sec}`
   return createDate
 }
 
